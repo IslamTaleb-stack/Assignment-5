e3b966b16461cb7c1dd8ef9d44cd940af9fb1cfd
IslamTaleb-stack <talebulislam126@gmail.com>
2026-09-12 22:43:09 +0600

    feat: build hero benner

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import Nav from '../src/components/Navbar'
-
+import Nav from '../src/components/Navbar';
+import Hero from '../src/components/Hero';
 
 function App() {
   return (
     <div>
       <Nav />
+      <Hero />
     </div>
   );
 }
---
+++ src/components/Hero.tsx
@@ -0,0 +1,32 @@
+// rsc
+import Banner from '../assets/banner-stack.png';
+
+const Hero = () => {
+    return (
+        <div>
+            <section className="container mx-auto px-4 py-16">
+                <div className="mx-auto flex max-w-6xl flex-col items-center gap-8 md:flex-row">
+                    
+                    {/* Hero Text */}
+                    <div className="flex-1">
+                        <h1 className="text-4xl font-bold">Build Your Ideal
+                            <br />
+                             <span className="bg-linear-to-r from-orange-400 via-pink-500 to-violet-600 bg-clip-text text-transparent">Development Stack</span>
+                        </h1>
+                       
+                        <p className="mt-4 text-base leading-7 text-gray-600">Explore frontend, backend, database, and tooling options, compare them side by side, and put together the stack that fits your next project.</p>
+                        <button className="mt-6 rounded-full bg-linear-to-r from-orange-400 via-pink-500 to-violet-600 px-6 py-3 font-semibold text-white">Explore Technologies</button>
+                        <button className="mt-6 ml-4 rounded-full border border-gray-300 px-6 py-3 font-semibold text-gray-700">Learn More</button>
+                    </div>
+
+                    {/* Hero Image */}
+                    <div className="flex-1">
+                        <img src={Banner} alt="Technology Stack" />
+                    </div>
+                </div>
+            </section>
+        </div>
+    );
+};
+
+export default Hero;
